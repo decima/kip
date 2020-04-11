a4fd96a22f603a8b9707ca02b96463fdea1f4c12
Alban <alban.crepel@gmail.com>
2020-04-12 01:57:23 +0200

    :ok_woman: use scss for style (and enabling webpack-encore) (#31)

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
